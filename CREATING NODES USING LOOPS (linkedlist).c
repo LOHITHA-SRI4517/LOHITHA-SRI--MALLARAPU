b774c4a4bfaf0d50fc9f38d7a7001037c88674b8
Lohitha Sri <smiley451745@gmail.com>
2026-03-12 15:52:50 +0530

    Add files via upload

---
+++ CREATING NODES USING LOOPS (linkedlist).c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+	int data;
+	struct node *link;  //self referential structures
+};
+int main(){
+	struct node *head=NULL,*temp=NULL;
+	int n,i;
+	printf("ENTER N VALUE:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+	struct node *newnode=(struct node*)malloc(sizeof(struct node));
+	printf("Enter node %d data:",i+1);
+	scanf("%d",&newnode->data);
+	newnode->link=NULL;
+	if (head==NULL){
+		head=newnode;
+		temp=newnode;
+	}
+	else{
+		temp->link=newnode;
+		temp=newnode;
+	}
+}
+	temp=head;
+	while(temp!=NULL){
+		printf("%d->\t",temp->data);	//traversing code
+		temp=temp->link;
+	}
+	return 0;
+}
+
